a8b929f7119f4fc3e46e6bd9aa15039bf36111a7
ШуфричWeb <51737588+spbkit1337@users.noreply.github.com>
2022-08-27 09:47:15 +0300

    update

---
+++ Исходный код/BoxInfoBeta/BoxInfoBeta/test/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace BoxInfoBeta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(200, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(56, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Видеокарта:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(56, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Процессор:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(180, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(56, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(236, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Температура твоего ПК";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 251);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkLabel1.Location = new System.Drawing.Point(121, 279);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(243, 22);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/spbkit1337";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(120, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Мой GitHub:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 335);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BoxInfoBeta";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Исходный код/BoxInfoBeta/BoxInfoBeta/test/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+//мои юзынги :)
+
+//Самая главная библеотека блогадаря которой возможно получить данные о температуре(и не только) процессора , видеокарты , материнки и т.д
+using OpenHardwareMonitor.Hardware; 
+using OpenHardwareMonitor.Collections;
+
+
+
+
+namespace BoxInfoBeta
+{
+    public partial class Form1 : Form
+    {
+        //Важное уточнение сначало я создал это приложение с названием test и потом решил поменять на BoxInfoBeta . Я поменял везде плюс надо было в файле program.cs добавить вот такую строку:  using BoxInfoBeta;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000; //я создал таймер чтобы каждую 1 сек обновлялся label с температурой
+            timer1.Start();
+
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //здесь я присвоил пустые значения для того чтобы при загрузке формы не было надписей label1 и 4 для красоты :)
+            label1.Text = "";
+            label4.Text = "";
+        }
+
+
+
+        //тут создаю переменные с плавающей запятой (float) для процессора и видюхи
+
+        // CPU Temperature
+        static float cpuTemp;
+
+        // GPU Temperature
+        static float gpuTemp;
+
+
+
+        //Таймер
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //определяем новый объект компьютера, предоставляемый библиотекой DLL Open Hardware Monitor
+            Computer myComputer = new Computer();
+
+
+            //Эти параметры указывают библиотеке Open Hardware Monitor возвращать данные для любых процессоров или видеокарт, подключенных в данный момент к системе
+            myComputer.GPUEnabled = true; 
+
+            myComputer.CPUEnabled = true;
+
+
+            //устанавливаем соединение между  кодом и файлом DLL
+            myComputer.Open();
+
+
+
+
+            //GPU видеокарта (с проверкой больше и меньше чтобы менять цвет)
+            foreach (var hardwareItem in myComputer.Hardware)
+            {
+                if (hardwareItem.HardwareType == HardwareType.GpuAti || hardwareItem.HardwareType == HardwareType.GpuNvidia)
+                {
+                    hardwareItem.Update();
+                    foreach (var sensor in hardwareItem.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            //присвавываем  переменной число с датчика
+                            gpuTemp = sensor.Value.GetValueOrDefault();
+
+                            //выводим в label
+                            //тут условие если температура меньше 50 то надпись будет зеленой
+                            if (gpuTemp < 50)
+                            {
+                                label1.ForeColor = Color.Green;
+                                label1.Text = "" + gpuTemp.ToString() + " °C";
+                            }
+                            else if (gpuTemp > 50) //если температура больше 50 то надпись будет красной
+                            {
+                                label1.ForeColor = Color.Red;
+                                label1.Text = "" + gpuTemp.ToString() + " °C";
+                            }
+
+                        }
+                    }
+                }
+            }
+
+
+            //CPU процессор (с проверкой больше и меньше чтобы менять цвет)
+            foreach (var hardware in myComputer.Hardware)
+            {
+                if (hardware.HardwareType == HardwareType.CPU)
+                {
+                    hardware.Update();
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            //присвавываем  переменной число с датчика
+                            cpuTemp = sensor.Value.GetValueOrDefault();
+
+                            //выводим в label
+                            //тут условие если температура меньше 50 то надпись будет зеленой
+                            if (cpuTemp < 50)
+                            {
+                                label4.ForeColor = Color.Green;
+                                label4.Text = "" + cpuTemp.ToString() + " °C";
+                            }
+                            else if (cpuTemp > 50) //если температура больше 50 то надпись будет красной
+                            {
+                                label4.ForeColor = Color.Red;
+                                label4.Text = "" + cpuTemp.ToString() + " °C";
+                            }
+
+                        }
+                    }
+                }
+            }
+
+
+            //Вывод температуры видеокарты с переменной
+            ////GPU
+            //foreach (var hardwareItem in myComputer.Hardware)
+            //{
+            //    if (hardwareItem.HardwareType == HardwareType.GpuAti || hardwareItem.HardwareType == HardwareType.GpuNvidia)
+            //    {
+            //        hardwareItem.Update();
+            //        foreach (var sensor in hardwareItem.Sensors)
+            //        {
+            //            if (sensor.SensorType == SensorType.Temperature)
+            //            {
+            //                //присвавываем  переменной число с датчика
+            //                gpuTemp = sensor.Value.GetValueOrDefault();
+
+            //                //выводим в label
+            //                label1.Text = "" + gpuTemp.ToString() + " °C";
+
+            //            }
+            //        }
+            //    }
+            //}
+
+
+
+            //Вывод температуры процессора с переменной
+            ////CPU
+            //foreach (var hardware in myComputer.Hardware)
+            //{
+            //    if (hardware.HardwareType == HardwareType.CPU)
+            //    {
+            //        hardware.Update();
+            //        foreach (var sensor in hardware.Sensors)
+            //        {
+            //            if (sensor.SensorType == SensorType.Temperature)
+            //            {
+            //                //присвавываем  переменной число с датчика
+            //                cpuTemp = sensor.Value.GetValueOrDefault();
+
+            //                //выводим в label
+            //                label4.Text = "" + cpuTemp.ToString() + " °C";
+
+            //            }
+            //        }
+            //    }
+            //}
+
+
+
+            //вывод температура видеокарты без переменных
+            ////GPU
+            //foreach (var hardwareItem in myComputer.Hardware)
+            //{
+
+            //    if (hardwareItem.HardwareType == HardwareType.GpuAti || hardwareItem.HardwareType == HardwareType.GpuNvidia)
+            //    {
+            //        foreach (var sensor in hardwareItem.Sensors)
+            //        {
+            //            if (sensor.SensorType == SensorType.Temperature)
+            //            {
+
+            //                label1.Text = " " + sensor.Value.GetValueOrDefault() + " °C";
+            //            }
+            //        }
+            //    }
+            //}
+
+
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/spbkit1337");
+        }
+    }
+
+
+    
+}
